9bae8e1e9c8a67dd1661050f12d45de5d856ddb1
Aleksandar Markovic <aleksandar.markovic@firstbeatmedia.com>
2021-06-16 21:55:29 +0200

    end Completing orders

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-redis/redis/v8 v8.10.0
 	github.com/gofiber/fiber/v2 v2.10.0
 	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
+	github.com/stripe/stripe-go/v72 v72.50.0
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
 	golang.org/x/text v0.3.6 // indirect
 	gorm.io/driver/postgres v1.1.0
--- src/controllers/orderController.go
+++ src/controllers/orderController.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"ambassador/src/database"
 	"ambassador/src/models"
+	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/checkout/session"
 )
 
 func Order(c *fiber.Ctx) error {
@@ -69,6 +72,8 @@ func CreateOrder(c *fiber.Ctx) error {
 		})
 	}
 
+	var lineItems []*stripe.CheckoutSessionLineItemParams
+
 	for _, requestProduct := range request.Products {
 		product := models.Product{}
 		product.Id = uint(requestProduct["product_id"])
@@ -91,7 +96,91 @@ func CreateOrder(c *fiber.Ctx) error {
 				"message": err.Error(),
 			})
 		}
+
+		lineItems = append(lineItems, &stripe.CheckoutSessionLineItemParams{
+			Name:        stripe.String(product.Title),
+			Description: stripe.String(product.Description),
+			Images:      []*string{stripe.String(product.Image)},
+			Amount:      stripe.Int64(100 * int64(product.Price)),
+			Currency:    stripe.String("usd"),
+			Quantity:    stripe.Int64(int64(requestProduct["quantity"])),
+		})
 	}
+	stripe.Key = "sk_test_51J33q8CSLk46JnomFwobrVfrttYdjQkb22X0ZNAS2GGf2gk0mAXfQFLbNjXcLeDjiH5nU8qvXJWqwrtHLAe3eqnA00rs47wR88"
+
+	params := stripe.CheckoutSessionParams{
+		SuccessURL:         stripe.String("http://localhost:5000/success?source={CHECKOUT_SESSION_ID}"),
+		CancelURL:          stripe.String("http://localhost:5000/error"),
+		PaymentMethodTypes: stripe.StringSlice([]string{"card"}),
+		LineItems:          lineItems,
+	}
+
+	source, err := session.New(&params)
+
+	if err != nil {
+		tx.Rollback()
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	order.TransactionId = source.ID
+
+	if err := tx.Save(&order).Error; err != nil {
+		tx.Rollback()
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
 	tx.Commit()
-	return c.JSON(order)
+	return c.JSON(source)
+}
+
+func CompleteOrder(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	order := models.Order{}
+
+	database.DB.Preload("OrderItems").First(&order, models.Order{
+		TransactionId: data["source"],
+	})
+
+	if order.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "Order not found",
+		})
+	}
+
+	order.Complete = true
+
+	database.DB.Save(&order)
+
+	go func(order models.Order) {
+		ambassadorRevenue := 0.0
+		adminRevenue := 0.0
+
+		for _, item := range order.OrderItems {
+			ambassadorRevenue += item.AmbassadorRevenue
+			adminRevenue += item.AdminRevenue
+		}
+		user := models.User{}
+		user.Id = order.UserId
+
+		database.DB.First(&user)
+
+		database.Cache.ZIncrBy(context.Background(), "rankings", ambassadorRevenue, user.Name())
+
+	}(order)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
